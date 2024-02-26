3a632b10869f94124c23b555a4850571eb8023f0
Yamkelani Velaphi <106350341+YamVel22@users.noreply.github.com>
2024-02-26 11:40:34 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+//Retrieve the values from the age input field and the membership checkbox.
+function checkEligibility() {
+  let age = document.getElementById("#age").value;
+  let legend = document.getElementById("#legend");
+  let premium = document.getElementById("#premium");
+  let junior = document.getElementById("#junior");
+  let result = document.getElementById("div.result");
 
-let form = 
-function checkEligibility() 
+  //Use conditional statements to check if the user is eligible for a discount based on criteria:
+  if (age >= 65) {
+    result.textContent =
+      "Congratulations! You are eligible for the Senior Discount.";
+  } else if (
+    age <= 18 &&
+    (legend.checked || premium.checked || junior.checked)
+  ) {
+    result.textContent =
+      "Congratulations! You are eligible for the Member Discount.";
+  } else {
+    result.textContent = "You are not eligible for a discount";
+  }
+}
+//Display the result in the output field or element.

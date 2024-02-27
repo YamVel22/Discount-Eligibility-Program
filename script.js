4055d6a25588ba02ac06085d56af4ff8011abc2d
Yamkelani Velaphi <106350341+YamVel22@users.noreply.github.com>
2024-02-27 10:56:26 +0200

    update

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 //Retrieve the values from the age input field and the membership checkbox.
 function checkEligibility() {
-  let age = document.getElementById("#age").value;
-  let legend = document.getElementById("#legend");
-  let premium = document.getElementById("#premium");
-  let junior = document.getElementById("#junior");
-  let result = document.getElementById("div.result");
+  let age = document.getElementById("age").value;
+  let legend = document.getElementById("legend").checked;
+  let premium = document.getElementById("premium").checked;
+  let junior = document.getElementById("junior").checked;
 
   //Use conditional statements to check if the user is eligible for a discount based on criteria:
+  let answer = "";
   if (age >= 65) {
-    result.textContent =
-      "Congratulations! You are eligible for the Senior Discount.";
-  } else if (
-    age <= 18 &&
-    (legend.checked || premium.checked || junior.checked)
-  ) {
-    result.textContent =
-      "Congratulations! You are eligible for the Member Discount.";
+    answer = "Congratulations! You are eligible for the Senior Discount.";
+  } else if (age <= 18 && (legend || premium || junior)) {
+    answer = "Congratulations! You are eligible for the Member Discount.";
   } else {
-    result.textContent = "You are not eligible for a discount";
+    answer = "You are not eligible for a discount";
   }
+
+  document.getElementById("answer").innerHTML = answer;
 }
-//Display the result in the output field or element.
